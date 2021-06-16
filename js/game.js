cd3cc334746cfca2ea5dfec6ec0eac16b7f8f86a
nipun <nipungarg.nipun@gmail.com>
2021-06-17 03:08:25 +0530

    newwerrrr changes

--- js/game.js
+++ js/game.js
@@ -63,7 +63,7 @@ let getNewQuestion = () => {
   if (availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
 
-    return window.location.assign("../end.html"); //redirect to end page
+    window.location.assign("end.html"); //redirect to end page
   } else {
     questionCounter++;
     progressText.innerHTML = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
