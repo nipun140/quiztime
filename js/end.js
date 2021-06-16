595b0a91c95f47d870df3b253c6fb232996f0d4a
nipun <nipungarg.nipun@gmail.com>
2021-06-17 02:35:25 +0530

    C:/Program Files/Git/ corrected

--- js/end.js
+++ js/end.js
@@ -26,7 +26,7 @@ saveBtn.onclick = () => {
     } else {
       names2dArr.push([inputName, score]);
       localStorage.setItem("leaderboard", JSON.stringify(names2dArr));
-      window.location.assign("/index.html");
+      window.location.assign("index.html");
       return;
     }
   }
